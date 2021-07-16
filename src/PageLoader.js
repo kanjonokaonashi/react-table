a242461e725cd25f0721f1ff08a08412eb54687f
Mary Prtavyan <kanjonokaonashi@gmail.com>
2021-07-16 15:55:56 +0400

    Fetch data into the store commit

---
+++ src/PageLoader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageLoader = (props) => {
+    return (
+            <div className="loader">
+                <h1>LOADING.....</h1>
+            </div>
+    )
+}
+
+export default PageLoader;
